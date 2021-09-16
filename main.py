05fc663115619365e6f7c7b6daf32e1b69326736
yuki <mansie.torsholt@gmail.com>
2021-09-16 20:47:04 +0200

    create question bank using my model on data.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-class Question:
+from question_model import Question
+from data import question_data
 
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+question_bank = []
 
+for dataset in question_data:
+    question = Question(dataset["text"], dataset["answer"])
+    question_bank.append(question)
+
+print(question_bank)
